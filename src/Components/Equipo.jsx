106eb03bd7df5d8c8c40235eabcfe550882d2596
Dennis <dennis.ferreyra@anima.edu.uy>
2024-12-06 09:18:32 -0300

    Se crearon rutas para la extraccion de datos de la API

--- src/Components/Equipo.jsx
+++ src/Components/Equipo.jsx
@@ -1,5 +1,10 @@
-const Equipo = () => {
-return <h1>Hi World!</h1>
+const Equipo = ({nombreEquipo, year, nombreEstadio, urlEquipo}) => {
+<div>
+    <p>{nombreEquipo}</p>
+    <p>{year}</p>
+    <p>{nombreEstadio}</p>
+    <img src={urlEquipo}></img>
+</div>
 
 }
 export { Equipo }
--- src/Pages/EquipoFavorito.jsx
+++ src/Pages/EquipoFavorito.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react"
+import { Equipo } from "../Components/Equipo"
+
 const EquipoFavorito = () => {
-    return  <h1>Esto es el EquipoFavorito </h1>
+    const [equipo, setEquipo] = useState({});
+    useEffect( () => {
+        async function obtenerDatos() {
+            const response = await fetch('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=Uruguayan%20Primera%20Division')
+            const data = await response.json();
+            setEquipo(data); 
+        }
+        obtenerDatos()
+    }, [] )
+    return (
+
+    <div>
+        <h1>Esto es el EquipoFavorito </h1>
+        <Equipo
+        nombreEquipo={"Racing"}
+        year={"1940"}
+        nombreEstadio={"Parque batlle"}
+        urlEquipo={"mikasa>>>"}
+        />
+    </div>
+    ) 
+
     
 }
 
